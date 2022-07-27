ded73d4e41290a5c8047877ef26f07bcbae68d2e
Simona Sriube <simona.martinkute3@gmail.com>
2022-07-27 14:56:12 +0300

    JS task 26_07_2022

---
+++ 26_07_2022/scripts/script.js
@@ -0,0 +1,28 @@
+alert('Hello World')
+
+let firstName = prompt('Please enter Your First Name?');
+console.log(firstName); 
+let lastName = prompt('Please enter Your Last Name?');
+console.log(lastName);
+
+const result = firstName + ' ' + lastName;
+console.log('Hello ' + result); 
+alert('Hello ' + result);
+
+let a = prompt('Give me the first value:');
+console.log('The fist value is ' + a);
+let b = prompt('Give me the second value:');
+console.log('The second value is ' + b);
+let result1= Number.parseInt(a) + Number.parseInt(b);
+console.log('The result of addition is ' + result1); 
+let result2= a - b;
+console.log('The result of substraction is ' + result2); 
+let result3= a * b;
+console.log('The result of multiplication is ' + result3); 
+let result4= a / b;
+console.log('The result of division is ' + result4); 
+
+alert('The result of addition is ' + result1);
+alert('The result of substraction is ' + result2);
+alert('The result of multiplication is ' + result3);
+alert('The result of division is ' + result4);
