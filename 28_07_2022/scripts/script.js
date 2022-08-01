68dc507a54045061ba3a7b5d2b7e3c500d7fb0b9
Simona Sriube <simona.martinkute3@gmail.com>
2022-08-01 13:04:25 +0300

    JS task 28_07_2022

---
+++ 28_07_2022/scripts/script.js
@@ -0,0 +1,44 @@
+let a = 5;
+let b = 10;
+console.log(`Fifteen is ${a + b} and not ${2 * a + b}`);
+// "Fifteen is 15 and
+// not 20"
+
+console.log(a == b);
+let c = 10;
+console.log(b == c);
+console.log(a = b);
+let cstr = "10";
+console.log(c == cstr);
+console.log(c === cstr);
+c = c + 2;
+c +=2;
+
+c = c * 2;
+c *= 2;
+
+c++;
+++c;
+
+console.log(c !== Number(cstr));
+console.log(c < 100);
+console.log(c++);
+console.log(c);
+
+// x - logical statement (retuens true or false)
+
+let x = true;
+
+if (x) {
+    // alert("a is true")
+} else {
+    // alert("a is false")
+};
+
+    
+if (x) {
+    // alert("a is true");
+} 
+if (!x){
+    // alert("a is false");
+};
