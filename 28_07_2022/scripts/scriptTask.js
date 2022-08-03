1128ebbd566280ae2bde16ddca601f2fc3fba73b
Simona Sriube <simona.martinkute3@gmail.com>
2022-08-03 09:52:36 +0300

    JS task 28_07_22_update

--- 28_07_2022/scripts/scriptTask.js
+++ 28_07_2022/scripts/scriptTask.js
@@ -4,37 +4,38 @@ console.log('The fist value is ' + x);
 let y = prompt('Give me the second value:');
 console.log('The second value is ' + y);
 
-let operator = prompt('Please choose what You would like to do: ADDITION (press " + ") or SUBSTRACTION (press " - ") or MULTIPLICATION (press " * ") or DIVISION (press " / ")')
+let operator = prompt('Please choose what You would like to do: ADDITION (press " + ") or SUBSTRACTION (press " - ") or MULTIPLICATION (press " * ") or DIVISION (press " / ")');
+console.log('The operator is ' + operator);
 
-
+let result;
 if ((operator !== '+' && operator !== '-' && operator !== '*' && operator !== '/') ) {
     console.log('You have choosen incorrect operator! Please take on of these : + - * / ')
     alert("You have choosen incorrect operator! Please take on of these : + - * / ");
 }else{ 
     if (!isNaN(x) && !isNaN(y)) {    
         if (operator == '+') {
-            let result1= Number.parseInt(x) + Number.parseInt(y);
-            console.log('The result of addition is ' + result1);
-            alert('The result of addition is ' + result1)
+            let result= Number(x) + Number(y);
+            console.log('The result of addition is ' + result);
+            alert('The result of addition is ' + result)
         } else if (operator == '-'){
-            let result2= x - y;
-            console.log('The result of substraction is ' + result2); 
-            alert('The result of substraction is ' + result2);
+            let result= Number(x) - Number(y);
+            console.log('The result of substraction is ' + result); 
+            alert('The result of substraction is ' + result);
         } else if (operator == '*'){
-            let result3= x * y;
-            console.log('The result of multiplication is ' + result3); 
-            alert('The result of multiplication is ' + result3)
+            let result= Number(x) * Number(y);
+            console.log('The result of multiplication is ' + result); 
+            alert('The result of multiplication is ' + result)
         } else {(operator == '/')
-            let result4= x / y;
-            console.log('The result of division is ' + result4); 
-            alert('The result of division is ' + result4);
+            let result= Number(x) / Number(y);
+            console.log('The result of division is ' + result); 
+            alert('The result of division is ' + result);
         } 
     }
     else {
         if (operator == '+') {
-            let result1= x + y;
-            console.log('The result of addition is string type value ' + result1);
-            alert('The result of addition is string type value ' + result1);
+            let result= x + y;
+            console.log('The result of addition is string type value ' + result);
+            alert('The result of addition is string type value ' + result);
         } else {
             console.log('Sorry but You can not use -, *, / for operations with string type values');
             alert('Sorry but You can not use -, *, / for operations with string type values')
