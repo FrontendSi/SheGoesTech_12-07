a303aa048a178ad07fb850ebf48b230a66c4e584
Simona Sriube <simona.martinkute3@gmail.com>
2022-08-16 12:45:11 +0300

    Calculator task

--- 09_08_2022/scripts/scripttask.js
+++ 09_08_2022/scripts/scripttask.js
@@ -2,29 +2,31 @@ function result(operator) {
     const value1 = document.getElementById("value1").value;
     const value2 = document.getElementById("value2").value;
     const result = document.getElementById("result");
-    result.innerHTML = "=" ;
+    const error = "The values provided should be the numbers";
 
     if (!isNaN(value1) && !isNaN(value2)) {
         if (operator == '+') {
-            let res = Number(value1) + Number(value2);
+            result.innerHTML = Number(value1) + Number(value2);
             console.log('The result of addition is ' + res);
         } else if (operator == '-') {
-            let res = Number(value1) - Number(value2);
+            result.innerHTML = Number(value1) - Number(value2);
             console.log('The result of substraction is ' + res);
         } else if (operator == '*') {
-            let res = Number(value1) * Number(value2);
+            result.innerHTML = Number(value1) * Number(value2);
             console.log('The result of multiplication is ' + res);
         } else {
             (operator == '/')
-            let res = Number(value1) / Number(value2);
+            result.innerHTML = Number(value1) / Number(value2);
             console.log('The result of division is ' + res);
         }
     }
     else {
         if (operator == '+') {
-            let res = value1 + value2;
-            console.log('The result of addition is string type value ' + res);
+            result.innerHTML = value1 + value2;
+            console.log('The result of addition is string type value ' + res)
+        }
+        else {
+            result.innerHTML = error;
         }
     }
-    return result.innerHTML = "="  ;
 }
