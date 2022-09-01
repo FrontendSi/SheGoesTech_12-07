f07e801b1776ab85b5f9435b59aee11d9a50e178
Simona Sriube <simona.martinkute3@gmail.com>
2022-09-01 14:19:04 +0300

    node and sql

--- my_app/frontend/scripts/script.js
+++ my_app/frontend/scripts/script.js
@@ -1,4 +1,8 @@
-$($.getJSON("http://localhost:5000/get-allCustomer",
+const customerIds =[];
+
+
+fetchSetCustomer = () => {
+    $.getJSON("http://localhost:5000/get-allCustomer",
     function(data){ 
         
         const customers = data.response.customers;
@@ -11,6 +15,7 @@ $($.getJSON("http://localhost:5000/get-allCustomer",
             const id = document.createElement("td");
             id.innerHTML = customer.id;
             row.append(id);
+            customerIds.push(customer.id);
 
             const firstName = document.createElement("td");
             firstName.innerHTML = customer.firstname;
@@ -35,4 +40,64 @@ $($.getJSON("http://localhost:5000/get-allCustomer",
             customersTabElement.append(row);
         }
 
-    }))
+    }).fail(() => {
+        console.clear();
+        setTimeout(
+        fetchSetCustomer(), 5000);
+    })
+};
+
+
+$(fetchSetCustomer());
+
+
+// ----- OPTION 1 ---
+// idValid = () => {
+//     const deleteIdElement = document.getElementById("deletedId");
+//     const valueEntered = Number(deleteIdElement.value);
+//     const isValid = customerIds.find((id) => {
+//         return id === valueEntered;
+//     }) !== undefined;
+//     if(isValid)
+//         deleteIdElement.setCustomValidity("");
+//     else
+//         deleteIdElement.setCustomValidity("ID does not exist");
+// }
+
+// ----- OPTION 2 with event listener ---
+
+
+// idValid = (event) => {
+//     const deleteIdElement = event.target;
+//     const valueEntered = Number(deleteIdElement.value);
+//     const isValid = customerIds.find((id) => {
+//         return id === valueEntered;
+//     }) !== undefined;
+//     if(isValid)
+//         deleteIdElement.setCustomValidity("");
+//     else
+//         deleteIdElement.setCustomValidity("ID does not exist");
+// }
+
+
+// ----- OPTION 3 with  add event listener ---
+
+
+$(document.addEventListener("input", (event) =>{
+    idValid(event);
+}))
+
+idValid = (event) => {
+    const deleteIdElement = event.target;
+    const valueEntered = Number(deleteIdElement.value);
+    const isValid = customerIds.find((id) => {
+        return id === valueEntered;
+    }) !== undefined;
+    if(isValid)
+        deleteIdElement.setCustomValidity("");
+    else
+        deleteIdElement.setCustomValidity("ID does not exist");
+}
+
+
+    
